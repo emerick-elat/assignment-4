cee0606c914e574073ed3e95b6af299d29a64d8a
Aymerick Fikeuelat <Aymerick.Fikeuelat@ventionteams.com>
2024-10-09 13:45:39 +0300

    Added audit api project

---
+++ Bank.AuditAPI/Controllers/V1/TransactionsController.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bank.AuditAPI.Controllers.V1
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public TransactionsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetTransactions([FromBody] object model)
+        {
+            return Ok();
+        }
+    }
+}
